364111ef9d15bca762e12d18c46701b3429b2b94
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-02-22 13:53:49 -0500

    added routes to app.jsx file

--- React-Portfolio/src/App.jsx
+++ React-Portfolio/src/App.jsx
@@ -1,7 +1,11 @@
+import {Route, Routes} from 'react-router-dom'
 import { useState } from 'react'
+
 import './App.css'
 import Header from './components/Header'
 import Project from './components/Project'
+import Home from './pages/Home'
+import About from './pages/About'
 import Footer from './components/Footer'
 import Contact from './pages/Contact'
 import { Outlet } from 'react-router-dom';
@@ -14,7 +18,12 @@ function App() {
     <>
     <Outlet />
     <Header />
-     <Footer />
+    <Footer />
+     <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
     </>
   )
 }
