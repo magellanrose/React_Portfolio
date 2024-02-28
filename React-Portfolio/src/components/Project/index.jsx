d9b84a26aa0fd821cebd5397f11fedb391d3f335
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-02-28 10:00:48 -0500

    added picture of me in about me

--- React-Portfolio/src/components/Project/index.jsx
+++ React-Portfolio/src/components/Project/index.jsx
@@ -5,12 +5,12 @@ function Project() {
   return (
     <>
       <div>
-          <h1>Projects</h1>
+        <h1>Projects</h1>
         <div>
           <Link to='https://magellanrose.github.io/Always_Sunny_Project/'>Always Sunny deployed link</Link>
           <Link to='https://github.com/magellanrose/Always_Sunny_Project'>Github Repo</Link>
-        <img src={coverImage1} className='imageHandler' />
-          
+          <img src={coverImage1} className='imageHandler' />
+
         </div>
       </div>
     </>
--- React-Portfolio/src/pages/About.jsx
+++ React-Portfolio/src/pages/About.jsx
@@ -1,8 +1,13 @@
+import coverImage1 from '../assets/IMG_2974.jpg';
+
+
 function About(){
   return(
     <>
        <h1>About Me</h1>
     <p> Hello world! I'm Nicholas Vacca, a passionate and aspiring junior developer currently enrolled in a rigorous coding bootcamp program. My journey into the world of coding is driven by a genuine love for problem-solving and a desire to create meaningful and impactful solutions through technology.</p>
+    <img src={coverImage1} className='imageHandler' />
+
     </>
    
    )
