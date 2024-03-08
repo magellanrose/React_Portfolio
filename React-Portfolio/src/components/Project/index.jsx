7e6a3d8667c32f0e325839970960168f33b4563f
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-03-08 17:07:49 -0500

    links on projects page, except the deplyed rink of recipe project

--- React-Portfolio/src/components/Project/index.jsx
+++ React-Portfolio/src/components/Project/index.jsx
@@ -7,20 +7,21 @@ import coverImage3 from '../../../../images/recipe.png';
 function Project() {
   return (
     <>
-    {/* div tag that contains an image of the deployed app, the title of the project, a link to the deployed app, and a link to the corresponding GitHub repo */}
+      {/* div tag that contains an image of the deployed app, the title of the project, a link to the deployed app, and a link to the corresponding GitHub repo */}
       <div>
         <h1>Projects</h1>
         <div>
           <Link to='https://magellanrose.github.io/Always_Sunny_Project/'>Always Sunny deployed link</Link>
           <Link to='https://github.com/magellanrose/Always_Sunny_Project'>Github Repo</Link>
           <img src={coverImage1} className='imageHandler' />
-        </div> <div>
-          <Link to='https://magellanrose.github.io/Always_Sunny_Project/'>Always Sunny deployed link</Link>
-          <Link to='https://github.com/magellanrose/Always_Sunny_Project'>Github Repo</Link>
+        </div>
+        <div>
+          <Link to='https://salty-coast-36768-7c025fc4e535.herokuapp.com/profile'>freeLances deployed link</Link>
+          <Link to='https://github.com/dann9109/freeflow'>Github Repo</Link>
           <img src={coverImage2} className='imageHandler' />
-        </div> <div>
-          <Link to='https://magellanrose.github.io/Always_Sunny_Project/'>Always Sunny deployed link</Link>
-          <Link to='https://github.com/magellanrose/Always_Sunny_Project'>Github Repo</Link>
+        </div>
+        <div>
+          <Link to='https://github.com/magellanrose/Recipe_Book_Project'>Github Repo</Link>
           <img src={coverImage3} className='imageHandler' />
         </div>
       </div>
